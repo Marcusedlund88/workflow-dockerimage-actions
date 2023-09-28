591284513934bcb45250714ed12fd319ecb17929
Marcus Edlund <marcus.edlund@yh.nackademin.se>
2023-09-28 11:13:59 +0200

    Updated Service class, implemented taskInstance

--- src/main/java/com/example/workflowdockerimageactions/SERVICE/TaskService.java
+++ src/main/java/com/example/workflowdockerimageactions/SERVICE/TaskService.java
@@ -1,5 +1,8 @@
 package com.example.workflowdockerimageactions.SERVICE;
 
+import com.example.workflowdockerimageactions.DATA.Task;
+import com.example.workflowdockerimageactions.MODEL.TaskInstance;
+import com.example.workflowdockerimageactions.REPO.TaskRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,5 +10,22 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TaskService {
 
+    private final TaskRepo taskRepo;
+    private Task newTask;
+    private TaskInstance taskInstance;
+
+    public TaskInstance createNewTask(String task, double timeInHours){
+
+        if(taskRepo.findTasksByName(task) != null){
+            taskInstance = new TaskInstance(taskRepo.findTasksByName(task),timeInHours);
+        }
+        else{
+            newTask = new Task();
+            newTask.setName(task);
+            taskRepo.save(newTask);
+            taskInstance = new TaskInstance(newTask,timeInHours);
+        }
+        return taskInstance;
+    }
 
 }
