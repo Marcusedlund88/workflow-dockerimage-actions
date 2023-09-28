9579d9eef88c3b7eae1ed5edb8bb378a173c08ba
Staz1x <fogel.stazix@gmail.com>
2023-09-28 15:51:27 +0200

    Added to taskTest

--- src/test/java/com/example/workflowdockerimageactions/DATA/TaskTest.java
+++ src/test/java/com/example/workflowdockerimageactions/DATA/TaskTest.java
@@ -1,10 +1,34 @@
 package com.example.workflowdockerimageactions.DATA;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
-class TaskTest {
+public final class TaskTest {
+
+    private Task testTask;
+
+    @BeforeEach
+    public void createTestTask() {
+        testTask = new Task("Task 1");
+    }
+
+
+    @Test
+    void getName_returnName(){
+        String expectedName = "Task 1";
+        String actualName = testTask.getName();
+        assertEquals(expectedName, actualName);
+    }
+
+
+    @Test
+    void setName_returnSetName(){
+        String newName = "Task 2";
+        testTask.setName(newName);
+        String actualName = testTask.getName();
+        assertEquals(newName, actualName);
+    }
 
 }
