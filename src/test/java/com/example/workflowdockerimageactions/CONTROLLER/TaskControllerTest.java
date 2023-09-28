185545271bcc2a2630667e3c25c968d0758d5c44
Marcus Edlund <marcus.edlund@yh.nackademin.se>
2023-09-28 14:30:39 +0200

    Service tests implemented

--- src/test/java/com/example/workflowdockerimageactions/CONTROLLER/TaskControllerTest.java
+++ src/test/java/com/example/workflowdockerimageactions/CONTROLLER/TaskControllerTest.java
@@ -1,10 +1,36 @@
 package com.example.workflowdockerimageactions.CONTROLLER;
 
-import org.springframework.boot.test.context.SpringBootTest;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+import java.util.Scanner;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TaskControllerTest {
 
+    @Test
+    void getAllTasks(){
+    }
+
+    @Test
+    void deleteTask() {
+    }
+
+    @Test
+    void addNewTask() {
+    }
 }
--- src/test/java/com/example/workflowdockerimageactions/SERVICE/TaskServiceTest.java
+++ src/test/java/com/example/workflowdockerimageactions/SERVICE/TaskServiceTest.java
@@ -1,10 +1,51 @@
 package com.example.workflowdockerimageactions.SERVICE;
 
+import com.example.workflowdockerimageactions.DATA.Task;
+import com.example.workflowdockerimageactions.MODEL.TaskInstance;
+import com.example.workflowdockerimageactions.REPO.TaskRepo;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TaskServiceTest {
 
+    @Autowired
+    private TaskRepo taskRepo;
+    @Autowired
+    private TaskService taskService;
+
+    private TaskInstance taskInstance;
+    private Task task;
+
+    @Test
+    void createNewTaskIfTaskExistsExpectTrue() {
+
+        task = new Task();
+        task.setName("Clean");
+        taskRepo.save(task);
+
+        taskInstance = taskService.createNewTask("Clean", 2);
+        assertEquals("Clean", taskInstance.getTask().getName());
+        assertEquals(2, taskInstance.getTimeInHours());
+    }
+    @Test
+    void createNewTaskIfTaskNotExistsExpectTrue() {
+
+        taskInstance = taskService.createNewTask("Wash", 1);
+        assertEquals("Wash", taskInstance.getTask().getName());
+        assertEquals(1, taskInstance.getTimeInHours());
+    }
 }
