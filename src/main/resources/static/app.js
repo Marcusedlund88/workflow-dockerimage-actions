e1ce6c8215868b00ab5519e2209c0e35bea82e8f
marcusH3 <marcus.hurtigh@outlook.com>
2023-09-28 17:25:43 +0200

    Date and time added to frontend.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,31 +1,47 @@
+document.querySelector('#push').onclick = function () {
+    var taskInput = document.querySelector('#newtask input[type="text"]');
+    var dateInput = document.querySelector('#taskDate');
+    var timeInput = document.querySelector('#taskTime');
 
-document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please Enter a Task")
-    }
-    else{
+    if (taskInput.value.length === 0) {
+        alert("Please enter a task.");
+    } else {
+        var taskName = taskInput.value;
+        var dateValue = dateInput.value;
+        var timeValue = timeInput.value;
+
+        var deadline = dateValue + ' ' + timeValue;
+
         document.querySelector('#tasks').innerHTML += `
-            <div class="task">
-                <span id="taskname">
-                    ${document.querySelector('#newtask input').value}
-                </span>
-                <button class="delete">
-                    <i class="far fa-trash-alt"></i>
-                </button>
-            </div>
-        `;
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
+      <div class="task">
+        <span id="taskname">
+          ${taskName}
+        </span>
+        <span id="taskDeadline">
+          Deadline: ${deadline}
+        </span>
+        <button class="delete">
+          <i class="far fa-trash-alt"></i>
+        </button>
+      </div>
+    `;
+
+        var currentTasks = document.querySelectorAll(".delete");
+        for (var i = 0; i < currentTasks.length; i++) {
+            currentTasks[i].onclick = function () {
                 this.parentNode.remove();
-            }
+            };
         }
+
         var tasks = document.querySelectorAll(".task");
-        for(var i=0; i<tasks.length; i++){
-            tasks[i].onclick = function(){
+        for (var i = 0; i < tasks.length; i++) {
+            tasks[i].onclick = function () {
                 this.classList.toggle('completed');
-            }
+            };
         }
-        document.querySelector("#newtask input").value = "";
+
+        taskInput.value = "";
+        dateInput.value = "";
+        timeInput.value = "";
     }
-}
+};
