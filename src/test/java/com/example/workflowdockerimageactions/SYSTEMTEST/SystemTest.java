4c7e4278c53db4eebe17f96b32df14bed094f1d4
Marcus Edlund <marcus.edlund@yh.nackademin.se>
2023-10-01 19:03:05 +0200

    System tests working. Integration tests working Re-packaging test classes
    TODO: connect frontend dockerize and push

--- src/test/java/com/example/workflowdockerimageactions/SYSTEMTEST/SystemTest.java
+++ src/test/java/com/example/workflowdockerimageactions/SYSTEMTEST/SystemTest.java
@@ -1,35 +1,23 @@
-package com.example.workflowdockerimageactions.CONTROLLER;
+package com.example.workflowdockerimageactions.SYSTEMTEST;
 
 
 import com.example.workflowdockerimageactions.COMPONENT.Component;
-import com.example.workflowdockerimageactions.DATA.Task;
 import com.example.workflowdockerimageactions.REPO.TaskRepo;
 import io.restassured.RestAssured;
-import netscape.javascript.JSObject;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.List;
-import java.util.Scanner;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class TaskControllerTest {
+class SystemTest {
 
     @Autowired
     private Component component;
@@ -37,16 +25,13 @@ class TaskControllerTest {
     @Autowired
     private TaskRepo taskRepo;
 
-    private static final Logger logger = LoggerFactory.getLogger(TaskControllerTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(SystemTest.class);
 
     @Test
     void getAllTasks(){
 
         String username = System.getenv("SS_USER");
         String password = System.getenv("SS_PASSWORD");
-
-        assertEquals(username, "user");
-
 
         RestAssured.baseURI = "http://localhost:8080";
 
